13b5014fe428c096ee72446c6455f74bd6b99c78
Juliana Reis <130945718+Ju-Reis@users.noreply.github.com>
2024-07-04 20:57:36 -0300

    Aula 11 JavaScript

---
+++ Aula11/Ex008.js
@@ -0,0 +1,13 @@
+///CONDIÇÃO SIMPLES (Quando não tem necessidade de usar o ELSE)
+
+var vel = 80
+
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel <= 60){
+    console.log('Dirija sempre usando o sinto de segurança!')  
+}
+
+console.log('Sua velocidade está acima do permitido. MULTADO!')
+
+
